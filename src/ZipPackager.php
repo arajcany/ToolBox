9b29becb86b0bfec120d63b4043b57d77c1ab7d0
arajcany <arajcany@users.noreply.github.com>
2022-07-01 11:04:04 +1000

    Update ZipPackager.php

--- src/ZipPackager.php
+++ src/ZipPackager.php
@@ -144,7 +144,7 @@ class ZipPackager
         }
 
         $factor = ($total / 100);
-        $currentFixed = max(100, intval(ceil($current / $factor)));
+        $currentFixed = min(100, intval(ceil($current / $factor)));
         $totalFixed = 100;
         if (empty($this->_progressBar)) {
             $this->io->out('');
